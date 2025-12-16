39d3e5f7d1da797f9aab8ad736924b917ffa4d9e
Uchenna Ihieri <uchennaihieri@yahoo.com>
2025-12-16 10:33:45 +0100

    Update Paths

--- components/data.js
+++ components/data.js
@@ -1,21 +1,21 @@
 const PathArray = [
     {
         pathName: "FINCA PAPERLESS",
-        pathDesc: "This tool enables the team to work effortlessly without needing physical document",
+        pathDesc: "This tool enables the back-office team to work effortlessly without needing physical document",
         pathLink: "https://apps.powerapps.com/play/e/4a05db9d-f7e4-e6cb-8831-e7cf74b7e415/a/859080c8-edd8-4140-8d14-464dccafc2a7?tenantId=f58ebc55-6569-4ee1-9284-a8e74c5a0a86&hint=006e4678-8a1a-4503-aecf-d3c9a7842448&sourcetime=1762514159472",
         pathImage: "./paperless_Image.png"
     },
     {
-        pathName: "OUTLOOK EMAIL",
-        pathDesc: "You can log into your email with ease from here. Useful when you cannot remember the email link",
-        pathLink: "https://outlook.office.com",
-        pathImage: "./email_image.png"
+        pathName: "FIELD FLOW",
+        pathDesc: "This tool enables field officers to capture account creation and loan application details digitally while in the field",
+        pathLink: "https://apps.powerapps.com/play/e/4a05db9d-f7e4-e6cb-8831-e7cf74b7e415/a/9f0ab40d-c75d-4c4a-b6e6-72507a082d86?tenantId=f58ebc55-6569-4ee1-9284-a8e74c5a0a86&hint=2b9675ba-ae18-4ab8-8f73-8d2d58c4099e&sourcetime=1765877011490",
+        pathImage: "./staffloan.png"
     },
     {
-        pathName: "STAFF LOAN",
-        pathDesc: "You can easily apply for a staff loan from here without needing manual forms or paperwork.",
-        pathLink: "https://fincaint.sharepoint.com/:l:/s/CROAgent/FEFCsrJV7eFJh0ftiI4Wv44BrfR5dHVZcj4QVqg9mp9oqQ?nav=NDU3N2FkMjctYjg1ZS00NWE5LWE2ZjctM2QyODhjMjQ3NDVm",
-        pathImage: "./staffloan.png"
+        pathName: "TELLER POINT",
+        pathDesc: "This tool allows branch tellers to collect deposits, withdrawals, and other teller requests digitally without paperwork",
+        pathLink: "https://apps.powerapps.com/play/e/4a05db9d-f7e4-e6cb-8831-e7cf74b7e415/a/bbebc72e-192b-44e1-8c42-fc0ac2be4cf6?tenantId=f58ebc55-6569-4ee1-9284-a8e74c5a0a86&hint=94baaebf-b10f-4efd-bc12-e0cf4d7e1935&sourcetime=1765877542807",
+        pathImage: "./teller.png"
     }
 ]
 
